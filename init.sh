1fd7fd4208fb3f189e4f8c1b500866c905435137
CETHER <robert.hct@gmail.com>
2016-09-10 12:47:45 -0500

    init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-#Install Apache
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-
-#Copying landing page
 wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/my-landing-page.html
 sudo cp my-landing-page.html /var/www/html/index.html
-wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/info.php
-sudo cp info.php /var/www/html/info.php
-wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/prueba.php
-sudo cp prueba.php /var/www/html/prueba.php
