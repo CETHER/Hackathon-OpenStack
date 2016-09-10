ebcb74cc9522be1737f8f8f21fb83f571f5eeab9
CETHER <robert.hct@gmail.com>
2016-09-10 12:01:55 -0500

    init

--- init.sh
+++ init.sh
@@ -5,14 +5,14 @@ sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
 #Install mysql
-echo mysql-server-5.1 mysql-server/root_password password pistones | debconf-set-selections
-echo mysql-server-5.1 mysql-server/root_password_again password pistones | debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password password raccoon | debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password_again password raccoon | debconf-set-selections
 apt-get install -y mysql-server
-dpkg --get-selections | grep mysql
+dpkg --get-seletions | grep mysql
 
-#CreateDB
-mysql -u root -ppistones -e "create database pistones"
-mysql -u root -ppistones -e "USE pistones; CREATE TABLE usuarios ( id_usuario int(6) NOT NULL, nombre varchar(50) NOT NULL, email varchar(50) NOT NULL, PRIMARY KEY (id_usuario) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+#CreatDB
+mysql -u root -praccoon -e "Create database raccoon"
+mysql -u root -praccoon -e "USE raccoon; CREATE TABLE usuarios ( id_usuario int(6) NOT NULL, nombre varchar(50) NOT NULL, email varchar(50) NOT NULL, PRIMARY KEY (id_usuario) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
 
 #Install php
 sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt php5-mysql -y
