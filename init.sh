9c9741c2bdf77f201f2e79dd6f34360a8ce01f42
CETHER <robert.hct@gmail.com>
2016-09-10 12:03:39 -0500

    init

--- init.sh
+++ init.sh
@@ -4,23 +4,6 @@
 sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-#Install mysql
-echo mysql-server-5.1 mysql-server/root_password password raccoon | debconf-set-selections
-echo mysql-server-5.1 mysql-server/root_password_again password raccoon | debconf-set-selections
-apt-get install -y mysql-server
-dpkg --get-seletions | grep mysql
-
-#CreatDB
-mysql -u root -praccoon -e "Create database raccoon"
-mysql -u root -praccoon -e "USE raccoon; CREATE TABLE usuarios ( id_usuario int(6) NOT NULL, nombre varchar(50) NOT NULL, email varchar(50) NOT NULL, PRIMARY KEY (id_usuario) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
-
-#Install php
-sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt php5-mysql -y
-sudo service apache2 restart
-
-#Install Composer
-sudo apt install composer -y
-composer require php-opencloud/openstack 
 
 #Copying landing page
 wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/my-landing-page.html
@@ -29,7 +12,3 @@ wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/info.ph
 sudo cp info.php /var/www/html/info.php
 wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/prueba.php
 sudo cp prueba.php /var/www/html/prueba.php
-
-#Working with phpOpenCloud
-#wget #https://raw.githubusercontent.com/rojovivo27/openstackTest/master/create.php
-#sudo cp create.php /var/www/html/create.php
