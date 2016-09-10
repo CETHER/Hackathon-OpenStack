3d7334c0b8f27f994576f7bd77922ff4c4107e7f
CETHER <robert.hct@gmail.com>
2016-09-10 13:30:22 -0500

    init_php_mysql

--- init.sh
+++ init.sh
@@ -8,6 +8,12 @@ echo mysql-server-5.1 mysql-server/root_password_again password raccoon | debcon
 apt-get install -y mysql-server
 dpkg --get-selections | grep mysql
 
+
+#CreateDB
+mysql -u root -praccoon -e "create database raccoon"
+mysql -u root -praccoon -e "USE raccoon; CREATE TABLE usuarios ( id_usuario int(6) NOT NULL, nombre varchar(50) NOT NULL, email varchar(50) NOT NULL, PRIMARY KEY (id_usuario) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+
+
 #Install php
 sudo apt-get install libapache2-mod-php5 php5 php5-mcrypt php5-mysql -y
 sudo service apache2 restart
