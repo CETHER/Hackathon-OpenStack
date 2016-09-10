556582e0ac29a065a6907b7d918732f93387139f
CETHER <robert.hct@gmail.com>
2016-09-10 02:55:28 -0500

    y

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install apache2
-sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt install apache2 -y
+sudo apt install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi php7.0 libapache2-mod-php7.0
 
 wget https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/my-landing-page.html
 sudo cp my-landing-page.html /var/www/html/index.html
