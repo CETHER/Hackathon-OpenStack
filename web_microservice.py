d5193b267ae66d072ef0d006ebe42a1926fc2cec
CETHER <robert.hct@gmail.com>
2016-09-11 12:42:10 -0500

    actualización final

--- web_microservice.py
+++ web_microservice.py
@@ -26,9 +26,9 @@ flavor_id = '2'
 flavor = conn.get_flavor(flavor_id)
 print(flavor)
 
-#paso-4 Ejecuta el init para montar servicios en servidor
+#28 paso-4 Ejecuta el init para montar servicios en servidor
 ex_userdata = '''#!/usr/bin/env bash
-curl -L -s https://raw.githubusercontent.com/openstack-hackathon/training-shade-cucea/master/challenge1/init.sh | bash -s --
+curl -L -s https://raw.githubusercontent.com/CETHER/Hackathon-OpenStack/master/init.sh | bash -s --
 '''
 
 #paso-5
@@ -50,14 +50,14 @@ else:
     conn.create_security_group(sec_group_name, 'network access for a web application.')
     conn.create_security_group_rule(sec_group_name, 80, 80, 'TCP')
 
-
-#paso 7 creación de instancia
+#paso 7 creacion de instancia
 print "\nServer creation:"
 instance_name = 'ROCKET-RACCOON'
 testing_instance = conn.create_server(wait=True, auto_ip=True,
     name=instance_name,
     image=image_id,
     flavor=flavor_id,
+    key_name='raccoon',
     userdata=ex_userdata,
     network=external_network,
     security_groups=[sec_group_name])
